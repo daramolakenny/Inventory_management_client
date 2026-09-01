5ce39dcb718a445c6f3d5a4eace80991634ff280
Daramola Kehinde <daramolakenny18@gmail.com>
2026-09-01 13:53:45 +0100

    new push

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import Manager from './pages/Manager.jsx'
 import Admin from './pages/Admin.jsx'
 import User from './pages/User.jsx'
 import Login from './components/Login.jsx'
-import Signup from './components/signup.jsx'
+import Signup from './components/Signup.jsx'
 
 
 createRoot(document.getElementById('root')).render(
